c296cc54c2de88d5566a0ad2dab3394f77977184
vamsi <vamsi@gmail>
2024-11-06 21:58:51 +0530

    clean login slice 06-11-24 21:58

--- redux/Features/Auth/LoginSlice.js
+++ redux/Features/Auth/LoginSlice.js
@@ -2,20 +2,6 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { API } from "../../../Constants/url";
 
-// Async function to get token from AsyncStorage
-export const loadToken = createAsyncThunk(
-  "auth/loadToken",
-  async (_, { rejectWithValue }) => {
-    try {
-      const token = await AsyncStorage.getItem("token");
-      return token ? JSON.parse(token) : null;
-    } catch (error) {
-      console.error("Failed to load token from AsyncStorage", error);
-      return rejectWithValue("Failed to load token");
-    }
-  }
-);
-
 // Async function for user login
 export const userLogin = createAsyncThunk(
   "auth/login",
@@ -43,8 +29,6 @@ const tokenSlice = createSlice({
     token: null,
     loading: true,
     error: null,
-    isSigningUp: false,
-    isLogin: false,
   },
   reducers: {
     logout: (state) => {
@@ -58,37 +42,19 @@ const tokenSlice = createSlice({
       state.token = action.payload;
       state.loading = false;
     },
-    setIsSigningUp: (state, action) => {
-      state.isSigningUp = action.payload;
-    },
-
     noToken: (state, action) => {
       state.loading = false;
     },
   },
   extraReducers: (builder) => {
     builder
-      .addCase(loadToken.pending, (state) => {
-        state.loading = true;
-      })
-      .addCase(loadToken.fulfilled, (state, action) => {
-        state.loading = false;
-        state.token = action.payload;
-        state.isLogin = true;
-      })
-      .addCase(loadToken.rejected, (state, action) => {
-        state.loading = false;
-        state.error = action.payload;
-        state.isLogin = false;
-      })
       .addCase(userLogin.pending, (state) => {
         state.loading = true;
       })
       .addCase(userLogin.fulfilled, (state, action) => {
         state.loading = false;
         state.token = action.payload.token;
-        state.isSigningUp = false;
-        state.isLogin = true;
+        state.error = null;
       })
       .addCase(userLogin.rejected, (state, action) => {
         state.loading = false;
@@ -98,7 +64,6 @@ const tokenSlice = createSlice({
   },
 });
 
-export const { logout, setIsSigningUp, setToken, setIsLogin, noToken } =
-  tokenSlice.actions;
+export const { logout, setToken, noToken } = tokenSlice.actions;
 
 export default tokenSlice.reducer;
